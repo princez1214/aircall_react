fb726e3aa6234794a3bf67fe3e6f448615e202ce
princez1214 <wxiao1214@gmail.com>
2021-10-22 04:28:30 +0800

    update the package.json command

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
     publicPath: "/"
   },
